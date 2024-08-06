a61ad626971b2b442d27f3fee2a85f4808567375
arun909 <arunbabu7591@gmail.com>
2024-08-06 19:55:37 +0530

    today

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose'); 
+const config = require('config'); 
+const db = config.get('mongoDB_URI'); 
+const connectDB = async() => { 
+    await mongoose.connect(db, { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true, 
+        useCreateIndex: true, 
+        useFindAndModify: false }); 
+console.log('MongoDB connected successfully!'); 
+}; 
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express'); 
-
+const connectDB = require('./config/db');
 const app = express(); 
 
 const PORT = process.env.PORT || 5000;
 
+// Connect to the database
+connectDB();
+
+// Define a simple route
 app.get('/', (req, res) => res.send('API running'));
 
-app.listen(PORT, () => console.log('Server started on port ${PORT}'));
+// Start the server
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
